e11db0b8dd569e8e66230e9d03afa57f4ede396a
Rahul_Kumar_Singh <rahul201.hitcseaiml2020@gmail.com>
2025-02-18 09:31:28 +0530

    add new file

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,10 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
    
 
     
-    // Contact form submission handling
     document.getElementById("contactForm").addEventListener("submit", function (e) {
         e.preventDefault();
-
+        sendMail(); // Call the sendMail function
+    
         // Create success message element
         let successMessage = document.createElement("div");
         successMessage.textContent = "Message sent successfully!";
@@ -31,32 +31,44 @@ document.addEventListener("DOMContentLoaded", function () {
         successMessage.style.textAlign = "center";
         successMessage.style.borderRadius = "5px";
         successMessage.id = "successMessage";
-
-        // Append the message after the form if not already present
+    
         let form = document.getElementById("contactForm");
         if (!document.getElementById("successMessage")) {
             form.appendChild(successMessage);
         }
-
-        // Remove the message after 3 seconds
+    
         setTimeout(() => {
             successMessage.remove();
         }, 3000);
-
-        // Reset form fields
-        this.reset();
     });
+    
+    function sendMail() {
+        var params = {
+            user_name: document.getElementById("name").value,
+            user_email: document.getElementById("email").value,
+            user_message: document.getElementById("message").value,
+        };
+    
+        const serviceID = "service_bh44mxy";
+        const templateID = "template_2meun9p";
+    
+        emailjs.send(serviceID, templateID, params)
+            .then(res => {
+                console.log(res);
+                // alert("Your message was sent successfully!");
+                document.getElementById("contactForm").reset();
+            })
+            .catch(err => console.log("Email sending failed:", err));
+    }
+    
+    
+
 });
 
-
-
+// ---------------brands--------------------
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
 
 
 let popup = document.getElementById("popup");
 
-function openPopup(){
-    popup.classList.add("open-popup");
-}
-function closePopup(){
-    popup.classList.remove("open-popup");
-}
